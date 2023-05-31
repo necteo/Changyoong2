e3c381f70c92e61e3b4280a23789ed94c47b3f75
necteo <cauresn06@gmail.com>
2023-05-31 20:16:29 +0900

    M

---
+++ src/main/generated/com/changyoong/ounmo/mapper/ExerciseRecordMapperImpl.java
@@ -0,0 +1,11 @@
+package com.changyoong.ounmo.mapper;
+
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-05-30T05:10:14+0900",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
+)
+public class ExerciseRecordMapperImpl implements ExerciseRecordMapper {
+}
