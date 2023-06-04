166443e4345af3dd307287880514f36ee724f5ca
necteo <cauresn06@gmail.com>
2023-06-05 02:15:51 +0900

    add: MapperImpl

---
+++ src/main/generated/com/changyoong/ounmo/mapper/BoardMapperImpl.java
@@ -0,0 +1,11 @@
+package com.changyoong.ounmo.mapper;
+
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-06-04T20:34:16+0900",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
+)
+public class BoardMapperImpl implements BoardMapper {
+}
---
+++ src/main/generated/com/changyoong/ounmo/mapper/CommentMapperImpl.java
@@ -0,0 +1,11 @@
+package com.changyoong.ounmo.mapper;
+
+import javax.annotation.processing.Generated;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2023-06-05T02:07:47+0900",
+    comments = "version: 1.5.3.Final, compiler: javac, environment: Java 17.0.2 (Oracle Corporation)"
+)
+public class CommentMapperImpl implements CommentMapper {
+}
